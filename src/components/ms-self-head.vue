56d9408bd3ca608f61a5ec21171424e51a83baba
onlyPeack <1454380130@qq.com>
2020-12-10 15:57:25 +0800

    优化

--- src/components/ms-self-head.vue
+++ src/components/ms-self-head.vue
@@ -1,206 +1,203 @@
 <template>
-  <div>
-    <nav class="nav" ref="ms-self-head" :style="navStyle">
-      <a href="./index.html">
-        <!--            <img src="../image/index/logo.png" alt="logo" class="logo"></a>-->
-        <img src="image/index/logo.png" alt="logo" class="logo"></a>
-      <ul class="nav_list">
-        <li><span><a href="./index.html">首页</a></span></li>
-        <li>
-          <el-dropdown :show-timeout=0>
+  <nav class="nav" ref="ms-self-head" v-bind:style="navStyle">
+    <a href="./index.html">
+      <!--            <img src="../image/index/logo.png" alt="logo" class="logo"></a>-->
+      <img src="image/index/logo.png" alt="logo" class="logo"></a>
+    <ul class="nav_list">
+      <li><span><a href="./index.html">首页</a></span></li>
+      <li>
+        <el-dropdown :show-timeout=0>
                   <span class="el-dropdown-link">
                     产品介绍
                   </span>
-            <el-dropdown-menu slot="dropdown">
-              <a href="./goods-erp-1.html">
-                <el-dropdown-item>云ERP</el-dropdown-item>
-              </a>
-              <a href="./goods-e-erp.html">
-                <el-dropdown-item>云电商</el-dropdown-item>
-              </a>
-              <a href="./goods-cloud-classRoom.html">
-                <el-dropdown-item>云课堂</el-dropdown-item>
-              </a>
-              <a href="./goods-wms.html">
-                <el-dropdown-item>云仓储</el-dropdown-item>
-              </a>
-            </el-dropdown-menu>
-          </el-dropdown>
-        </li>
-        <li>
-          <el-dropdown :show-timeout=0>
+          <el-dropdown-menu slot="dropdown">
+            <a href="./goods-erp-1.html">
+              <el-dropdown-item>云ERP</el-dropdown-item>
+            </a>
+            <a href="./goods-e-erp.html">
+              <el-dropdown-item>云电商</el-dropdown-item>
+            </a>
+            <a href="./goods-cloud-classRoom.html">
+              <el-dropdown-item>云课堂</el-dropdown-item>
+            </a>
+            <a href="./goods-wms.html">
+              <el-dropdown-item>云仓储</el-dropdown-item>
+            </a>
+          </el-dropdown-menu>
+        </el-dropdown>
+      </li>
+      <li>
+        <el-dropdown :show-timeout=0>
               <span class="el-dropdown-link">
                 解决方案
               </span>
-            <el-dropdown-menu slot="dropdown">
-              <a href="./project-erp.html">
-                <el-dropdown-item>电商ERP一体化</el-dropdown-item>
-              </a>
-              <a href="./project-saas.html">
-                <el-dropdown-item>Saas协同云服务</el-dropdown-item>
-              </a>
-              <a href="./project-wms.html">
-                <el-dropdown-item>WMS智能仓储</el-dropdown-item>
-              </a>
-              <a href="./project-make.html">
-                <el-dropdown-item>生产制造ERP</el-dropdown-item>
-              </a>
-            </el-dropdown-menu>
-          </el-dropdown>
-        </li>
-        <li>
-          <el-dropdown :show-timeout=0>
+          <el-dropdown-menu slot="dropdown">
+            <a href="./project-erp.html">
+              <el-dropdown-item>电商ERP一体化</el-dropdown-item>
+            </a>
+            <a href="./project-saas.html">
+              <el-dropdown-item>Saas协同云服务</el-dropdown-item>
+            </a>
+            <a href="./project-wms.html">
+              <el-dropdown-item>WMS智能仓储</el-dropdown-item>
+            </a>
+            <a href="./project-make.html">
+              <el-dropdown-item>生产制造ERP</el-dropdown-item>
+            </a>
+          </el-dropdown-menu>
+        </el-dropdown>
+      </li>
+      <li>
+        <el-dropdown :show-timeout=0>
               <span class="el-dropdown-link">
                 行业赋能
               </span>
-            <el-dropdown-menu slot="dropdown">
-              <a href="./industry-pim.html">
-                <el-dropdown-item>PIM数据中台</el-dropdown-item>
-              </a>
-              <a href="./industry-wit.html">
-                <el-dropdown-item>工业智库</el-dropdown-item>
-              </a>
-              <a href="./industry-integration.html">
-                <el-dropdown-item>工业物联集成</el-dropdown-item>
-              </a>
-              <a href="./industry-operator.html">
-                <el-dropdown-item>工业电商代运营</el-dropdown-item>
-              </a>
-            </el-dropdown-menu>
-          </el-dropdown>
-        </li>
-        <li><a href="./about-us.html"><span>关于我们</span></a></li>
-        <!--            <li style="width: 100px">-->
-        <!--                <button>登录</button>-->
-        <!--            </li>-->
-        <!--            <li style="width: 100px;margin-left: 15px">-->
-        <!--                <button>注册</button>-->
-        <!--            </li>-->
-      </ul>
-      <i class="iconfont menuBtn" v-if="!isShow" @click="isShow=!isShow">&#xe653;</i>
-      <i class="iconfont menuBtn" v-else @click="isShow=!isShow"
-         style="font-size: 2em;margin-right: 5vw;">&#xe678;</i>
-      <el-collapse-transition>
-        <el-menu default-active="2"
-                 v-if="isShow"
-                 style="position: absolute;top: 15.42vw;width: 100%"
-                 class="el-menu-vertical-demo">
-          <el-menu-item index="1">
-            <a href="./index.html" slot="title">
-              <div>首页</div>
+          <el-dropdown-menu slot="dropdown">
+            <a href="./industry-pim.html">
+              <el-dropdown-item>PIM数据中台</el-dropdown-item>
+            </a>
+            <a href="./industry-wit.html">
+              <el-dropdown-item>工业智库</el-dropdown-item>
+            </a>
+            <a href="./industry-integration.html">
+              <el-dropdown-item>工业物联集成</el-dropdown-item>
+            </a>
+            <a href="./industry-operator.html">
+              <el-dropdown-item>工业电商代运营</el-dropdown-item>
+            </a>
+          </el-dropdown-menu>
+        </el-dropdown>
+      </li>
+      <li><a href="./about-us.html"><span>关于我们</span></a></li>
+      <!--            <li style="width: 100px">-->
+      <!--                <button>登录</button>-->
+      <!--            </li>-->
+      <!--            <li style="width: 100px;margin-left: 15px">-->
+      <!--                <button>注册</button>-->
+      <!--            </li>-->
+    </ul>
+    <i class="iconfont menuBtn" v-if="!isShow" @click="isShow=!isShow">&#xe653;</i>
+    <i class="iconfont menuBtn" v-else @click="isShow=!isShow"
+       style="font-size: 2em;margin-right: 5vw;">&#xe678;</i>
+    <el-collapse-transition>
+      <el-menu default-active="2"
+               v-if="isShow"
+               style="position: absolute;top: 15.42vw;width: 100%"
+               class="el-menu-vertical-demo">
+        <el-menu-item index="1">
+          <a href="./index.html" slot="title">
+            <div>首页</div>
+          </a>
+        </el-menu-item>
+        <el-submenu index="2">
+          <template slot="title">
+            <span>产品</span>
+          </template>
+          <el-menu-item index="2-1">
+            <a href="./goods-erp-1.html" slot="title">
+              <ul style="list-style-type:disc;">
+                <li>云ERP</li>
+              </ul>
             </a>
           </el-menu-item>
-          <el-submenu index="2">
-            <template slot="title">
-              <span>产品</span>
-            </template>
-            <el-menu-item index="2-1">
-              <a href="./goods-erp-1.html" slot="title">
-                <ul style="list-style-type:disc;">
-                  <li>云ERP</li>
-                </ul>
-              </a>
-            </el-menu-item>
-            <el-menu-item index="2-2">
-              <a href="./goods-e-erp.html" slot="title">
-                <ul style="list-style-type:disc;">
-                  <li>云电商</li>
-                </ul>
-              </a>
-            </el-menu-item>
-            <el-menu-item index="2-3">
-              <a href="./goods-cloud-classRoom.html" slot="title">
-                <ul style="list-style-type:disc;">
-                  <li>云课堂</li>
-                </ul>
-              </a>
-            </el-menu-item>
-            <el-menu-item index="2-4">
-              <a href="./goods-wms.html" slot="title">
-                <ul style="list-style-type:disc;">
-                  <li>云仓储</li>
-                </ul>
-              </a>
-            </el-menu-item>
-          </el-submenu>
-          <el-submenu index="3">
-            <template slot="title">
-              <span>解决方案</span>
-            </template>
-            <el-menu-item index="3-1">
-              <a href="./project-erp.html" slot="title">
-                <ul style="list-style-type:disc;">
-                  <li>电商ERP一体化</li>
-                </ul>
-              </a>
-            </el-menu-item>
-            <el-menu-item index="3-2">
-              <a href="./project-saas.html" slot="title">
-                <ul style="list-style-type:disc;">
-                  <li>Saas协同云服务</li>
-                </ul>
-              </a>
-            </el-menu-item>
-            <el-menu-item index="3-3">
-              <a href="./project-wms.html" slot="title">
-                <ul style="list-style-type:disc;">
-                  <li>WMS智能仓储</li>
-                </ul>
-              </a>
-            </el-menu-item>
-            <el-menu-item index="3-4">
-              <a href="./project-make.html" slot="title">
-                <ul style="list-style-type:disc;">
-                  <li>生产制造ERP</li>
-                </ul>
-              </a>
-            </el-menu-item>
-          </el-submenu>
-          <el-submenu index="4">
-            <template slot="title">
-              <span>行业赋能</span>
-            </template>
-            <el-menu-item index="4-1">
-              <a href="./industry-pim.html" slot="title">
-                <ul style="list-style-type:disc;">
-                  <li>PIM数据中台</li>
-                </ul>
-              </a>
-            </el-menu-item>
-            <el-menu-item index="4-2">
-              <a href="./industry-wit.html" slot="title">
-                <ul style="list-style-type:disc;">
-                  <li>工业智库</li>
-                </ul>
-              </a>
-            </el-menu-item>
-            <el-menu-item index="4-3">
-              <a href="./industry-integration.html" slot="title">
-                <ul style="list-style-type:disc;">
-                  <li>工业物联集成</li>
-                </ul>
-              </a>
-            </el-menu-item>
-            <el-menu-item index="4-4">
-              <a href="./industry-operator.html" slot="title">
-                <ul style="list-style-type:disc;">
-                  <li>工业电商代运营</li>
-                </ul>
-              </a>
-            </el-menu-item>
-          </el-submenu>
-
-          <el-menu-item index="4">
-            <a href="./about-us.html" slot="title">
-              <div>关于我们</div>
+          <el-menu-item index="2-2">
+            <a href="./goods-e-erp.html" slot="title">
+              <ul style="list-style-type:disc;">
+                <li>云电商</li>
+              </ul>
             </a>
           </el-menu-item>
-        </el-menu>
-      </el-collapse-transition>
+          <el-menu-item index="2-3">
+            <a href="./goods-cloud-classRoom.html" slot="title">
+              <ul style="list-style-type:disc;">
+                <li>云课堂</li>
+              </ul>
+            </a>
+          </el-menu-item>
+          <el-menu-item index="2-4">
+            <a href="./goods-wms.html" slot="title">
+              <ul style="list-style-type:disc;">
+                <li>云仓储</li>
+              </ul>
+            </a>
+          </el-menu-item>
+        </el-submenu>
+        <el-submenu index="3">
+          <template slot="title">
+            <span>解决方案</span>
+          </template>
+          <el-menu-item index="3-1">
+            <a href="./project-erp.html" slot="title">
+              <ul style="list-style-type:disc;">
+                <li>电商ERP一体化</li>
+              </ul>
+            </a>
+          </el-menu-item>
+          <el-menu-item index="3-2">
+            <a href="./project-saas.html" slot="title">
+              <ul style="list-style-type:disc;">
+                <li>Saas协同云服务</li>
+              </ul>
+            </a>
+          </el-menu-item>
+          <el-menu-item index="3-3">
+            <a href="./project-wms.html" slot="title">
+              <ul style="list-style-type:disc;">
+                <li>WMS智能仓储</li>
+              </ul>
+            </a>
+          </el-menu-item>
+          <el-menu-item index="3-4">
+            <a href="./project-make.html" slot="title">
+              <ul style="list-style-type:disc;">
+                <li>生产制造ERP</li>
+              </ul>
+            </a>
+          </el-menu-item>
+        </el-submenu>
+        <el-submenu index="4">
+          <template slot="title">
+            <span>行业赋能</span>
+          </template>
+          <el-menu-item index="4-1">
+            <a href="./industry-pim.html" slot="title">
+              <ul style="list-style-type:disc;">
+                <li>PIM数据中台</li>
+              </ul>
+            </a>
+          </el-menu-item>
+          <el-menu-item index="4-2">
+            <a href="./industry-wit.html" slot="title">
+              <ul style="list-style-type:disc;">
+                <li>工业智库</li>
+              </ul>
+            </a>
+          </el-menu-item>
+          <el-menu-item index="4-3">
+            <a href="./industry-integration.html" slot="title">
+              <ul style="list-style-type:disc;">
+                <li>工业物联集成</li>
+              </ul>
+            </a>
+          </el-menu-item>
+          <el-menu-item index="4-4">
+            <a href="./industry-operator.html" slot="title">
+              <ul style="list-style-type:disc;">
+                <li>工业电商代运营</li>
+              </ul>
+            </a>
+          </el-menu-item>
+        </el-submenu>
 
-    </nav>
-  </div>
+        <el-menu-item index="4">
+          <a href="./about-us.html" slot="title">
+            <div>关于我们</div>
+          </a>
+        </el-menu-item>
+      </el-menu>
+    </el-collapse-transition>
 
+  </nav>
 </template>
 
 <script>
@@ -216,14 +213,14 @@
           style:[
             {
               label:'背景色',
-              name:'background-color',
+              name:'backgroundColor',
               value:'rgba(0,0,0,0)',
               // value:'red',
               index:0
             },
             {
               label:'滚动时背景色',
-              name:'background-color',
+              name:'backgroundColor',
               value:'#1E2327',
               index:1
             }
@@ -266,7 +263,7 @@
       },
       computedNavStyle(){
         let style=[{
-          "background-color":this.module.style[0].value
+          "backgroundColor":this.module.style[0].value
         }]
         for (let i = 2; i <this.module.style.length ; i++) {
           style.push({
@@ -274,6 +271,7 @@
           })
         }
         this.navStyle=style
+        console.log(this.navStyle,'style')
       }
     }
   }
