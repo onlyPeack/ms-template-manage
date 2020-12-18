7a2179f2d4617b4f00b9bd16d4c25ff1c98033ac
onlyPeack <1454380130@qq.com>
2020-12-18 17:26:38 +0800

    update

--- src/components/ms-self-head.vue
+++ src/components/ms-self-head.vue
@@ -185,7 +185,7 @@
               label: '文字组合1',
               children: [
                 {
-                  label: '首页1',
+                  label: '首页2',
                   link: './index.html'
                 },
                 {
